5c7bf2d666f612ae5e7a0828b5d16f1c87e7903e
chanhoyee <82632180+chanhoyee@users.noreply.github.com>
2021-06-04 23:40:30 +0900

    Update 3_nonlinear_ML.py

--- 3_nonlinear_ML.py
+++ 3_nonlinear_ML.py
@@ -1,6 +1,6 @@
 def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest, ps):
 
-    
+    '''
     # RF 
     param_grid_RF = {
         'bootstrap': [True],
@@ -26,8 +26,7 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
     grid_GBRT = GridSearchCV(estimator=GBRT, param_grid=param_grid_GBRT, cv=ps, verbose=2)
     grid_GBRT.fit(Xtrainval, ytrainval)
     ytest_hat_GBRT = grid_GBRT.predict(Xtest) 
-    
-    
+    '''
     # NNs
     XtrainNN = torch.FloatTensor(Xtrain).to('cuda')
     ytrainNN = torch.FloatTensor(ytrain.squeeze()).to('cuda')
@@ -39,7 +38,7 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
     print(XvalNN.shape,yvalNN.shape)
     print(XtestNN.shape,ytestNN.shape)
     epochs = 50000
-    
+    gc.disable()
     # NN1 
     class NN1(nn.Module):
         def __init__(self, n_features, NN1_layer1_Range):
@@ -50,7 +49,7 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
         def forward(self, x):
             x = F.relu(self.fc1(x))
             return self.fc2(x)
-    NN1_layer1_Range = np.array([2, 4, 8, 16, 32, 64, 128])
+    NN1_layer1_Range = np.array([2, 4, 8, 16, 32, 64])
     resultsCV = pd.DataFrame(columns=['layer1_Range'])
     for i in range(NN1_layer1_Range.size):            
         num_layer1 = NN1_layer1_Range[i]
@@ -59,6 +58,7 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
         criterion = nn.MSELoss() 
         optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
         for epoch in range(epochs):
+            gc.disable()
             ytrainNN_hat = model(XtrainNN)
             loss = criterion(torch.squeeze(ytrainNN_hat),ytrainNN)
             print(f'NN1: {i} Epoch: {epoch} Loss: {loss}')
@@ -68,9 +68,9 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
             optimizer.step() 
             if epoch == 0:
                 loss_past = loss
-            elif abs(loss_past-loss)>1e-10:
+            elif abs(loss_past-loss)>1e-9:
                     loss_past = loss
-            elif abs(loss_past-loss)<=1e-10:
+            elif abs(loss_past-loss)<=1e-9:
                     break
         yvalNN_hat = model(XvalNN)
         val_loss = criterion(torch.squeeze(yvalNN_hat),yvalNN).cpu().detach().numpy()
@@ -99,8 +99,8 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
             x = F.relu(self.fc1(x))
             x = F.relu(self.fc2(x))
             return self.fc3(x)        
-    NN2_layer1_Range = np.array([2, 4, 8, 16, 32, 64, 128])
-    NN2_layer2_Range = np.array([2, 4, 8, 16, 32, 64, 128])
+    NN2_layer1_Range = np.array([2, 4, 8, 16, 32, 64])
+    NN2_layer2_Range = np.array([2, 4, 8, 16, 32, 64])
     resultsCV = pd.DataFrame(columns=['layer1_Range', 'layer2_Range'])
     for i in range(NN2_layer1_Range.size):
         for j in range(NN2_layer2_Range.size):            
@@ -111,6 +111,7 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
             criterion = nn.MSELoss() 
             optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
             for epoch in range(epochs):
+                gc.disable()
                 ytrainNN_hat = model(XtrainNN)
                 loss = criterion(torch.squeeze(ytrainNN_hat),ytrainNN)
                 print(f'NN2: {i,j} Epoch: {epoch} Loss: {loss}')
@@ -120,9 +121,9 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
                 optimizer.step()
                 if epoch == 0:
                     loss_past = loss
-                elif abs(loss_past-loss)>1e-10:
+                elif abs(loss_past-loss)>1e-9:
                         loss_past = loss
-                elif abs(loss_past-loss)<=1e-10:
+                elif abs(loss_past-loss)<=1e-9:
                         break
             yvalNN_hat = model(XvalNN)
             val_loss = criterion(torch.squeeze(yvalNN_hat),yvalNN).cpu().detach().numpy()
@@ -154,9 +155,9 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
             x = F.relu(self.fc2(x))
             x = F.relu(self.fc3(x))
             return self.fc4(x)        
-    NN3_layer1_Range = np.array([2, 4, 8, 16, 32, 64, 128])
-    NN3_layer2_Range = np.array([2, 4, 8, 16, 32, 64, 128])
-    NN3_layer3_Range = np.array([2, 4, 8, 16, 32, 64, 128])
+    NN3_layer1_Range = np.array([2, 4, 8, 16, 32, 64])
+    NN3_layer2_Range = np.array([2, 4, 8, 16, 32, 64])
+    NN3_layer3_Range = np.array([2, 4, 8, 16, 32, 64])
     resultsCV = pd.DataFrame(columns=['layer1_Range', 'layer2_Range', 'layer3_Range'])
     for i in range(NN3_layer1_Range.size):
         for j in range(NN3_layer2_Range.size):            
@@ -167,8 +168,9 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
                 model = NN3(XtrainNN.shape[1], num_layer1, num_layer2, num_layer3).to('cuda')
                 # loss function that measures difference between two binary vectors
                 criterion = nn.MSELoss() 
-                optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+                optimizer = torch.optim.Adam(model.parameters(), lr = 0.001, weight_decay=0.995)
                 for epoch in range(epochs):
+                    gc.disable()
                     ytrainNN_hat = model(XtrainNN)
                     loss = criterion(torch.squeeze(ytrainNN_hat),ytrainNN)
                     print(f'NN3: {i,j,k} Epoch: {epoch} Loss: {loss}')
@@ -178,9 +180,9 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
                     optimizer.step()
                     if epoch == 0:
                         loss_past = loss
-                    elif abs(loss_past-loss)>1e-10:
+                    elif abs(loss_past-loss)>1e-9:
                             loss_past = loss
-                    elif abs(loss_past-loss)<=1e-10:
+                    elif abs(loss_past-loss)<=1e-9:
                             break
                 yvalNN_hat = model(XvalNN)
                 val_loss = criterion(torch.squeeze(yvalNN_hat),yvalNN).cpu().detach().numpy()
@@ -200,6 +202,8 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
     print(resultsCV_NN3)
     ytest_hat_NN3 = model_temp(XtestNN).cpu().detach().numpy()
     
+    
+    '''
     # NN4 
     class NN4(nn.Module):
         def __init__(self, n_features, NN4_layer1_Range, NN4_layer2_Range, NN4_layer3_Range, NN4_layer4_Range):
@@ -215,10 +219,10 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
             x = F.relu(self.fc3(x))
             x = F.relu(self.fc4(x))
             return self.fc5(x)        
-    NN4_layer1_Range = np.array([2, 4, 8, 16, 32])
-    NN4_layer2_Range = np.array([2, 4, 8, 16, 32])
-    NN4_layer3_Range = np.array([2, 4, 8, 16, 32])
-    NN4_layer4_Range = np.array([2, 4, 8, 16, 32])
+    NN4_layer1_Range = np.array([2, 4, 8, 16])
+    NN4_layer2_Range = np.array([2, 4, 8, 16])
+    NN4_layer3_Range = np.array([2, 4, 8, 16])
+    NN4_layer4_Range = np.array([2, 4, 8, 16])
     resultsCV = pd.DataFrame(columns=['layer1_Range', 'layer2_Range', 'layer3_Range', 'layer4_Range'])
     for i in range(NN4_layer1_Range.size):
         for j in range(NN4_layer2_Range.size):            
@@ -233,6 +237,7 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
                     criterion = nn.MSELoss() 
                     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
                     for epoch in range(epochs):
+                        gc.disable()
                         ytrainNN_hat = model(XtrainNN)
                         loss = criterion(torch.squeeze(ytrainNN_hat),ytrainNN)
                         print(f'NN4: {i,j,k,l} Epoch: {epoch} Loss: {loss}')
@@ -242,9 +247,9 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
                         optimizer.step()
                         if epoch == 0:
                             loss_past = loss
-                        elif abs(loss_past-loss)>1e-10:
+                        elif abs(loss_past-loss)>1e-9:
                                 loss_past = loss
-                        elif abs(loss_past-loss)<=1e-10:
+                        elif abs(loss_past-loss)<=1e-9:
                                 break
                     yvalNN_hat = model(XvalNN)
                     val_loss = criterion(torch.squeeze(yvalNN_hat),yvalNN).cpu().detach().numpy()
@@ -283,10 +288,10 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
             x = F.relu(self.fc5(x))
             return self.fc6(x)        
     NN5_layer1_Range = np.array([2, 4, 8, 16, 32])
-    NN5_layer2_Range = np.array([2, 4, 8, 16, 32])
-    NN5_layer3_Range = np.array([2, 4, 8, 16, 32])
-    NN5_layer4_Range = np.array([2, 4, 8, 16, 32])
-    NN5_layer5_Range = np.array([2, 4, 8, 16, 32])
+    NN5_layer2_Range = np.array([2, 4, 8, 16])
+    NN5_layer3_Range = np.array([2, 4, 8, 16])
+    NN5_layer4_Range = np.array([2, 4, 8, 16])
+    NN5_layer5_Range = np.array([2, 4, 8, 16])
     resultsCV = pd.DataFrame(columns=['layer1_Range', 'layer2_Range', 'layer3_Range', 'layer4_Range', 'layer5_Range'])
     for i in range(NN5_layer1_Range.size):
         for j in range(NN5_layer2_Range.size):            
@@ -303,6 +308,7 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
                         criterion = nn.MSELoss() 
                         optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
                         for epoch in range(epochs):
+                            gc.disable()
                             ytrainNN_hat = model(XtrainNN)
                             loss = criterion(torch.squeeze(ytrainNN_hat),ytrainNN)
                             print(f'NN5: {i,j,k,l,m} Epoch: {epoch} Loss: {loss}')
@@ -312,9 +318,9 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
                             optimizer.step()
                             if epoch == 0:
                                 loss_past = loss
-                            elif abs(loss_past-loss)>1e-10:
+                            elif abs(loss_past-loss)>1e-9:
                                     loss_past = loss
-                            elif abs(loss_past-loss)<=1e-10:
+                            elif abs(loss_past-loss)<=1e-9:
                                     break
                         yvalNN_hat = model(XvalNN)
                         val_loss = criterion(torch.squeeze(yvalNN_hat),yvalNN).cpu().detach().numpy()
@@ -335,5 +341,7 @@ def nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest,
     resultsCV_NN5 = resultsCV.sort_values('CV_Score',ascending=True)
     print(resultsCV_NN5)
     ytest_hat_NN5 = model_temp(XtestNN).cpu().detach().numpy()
+    '''
     torch.cuda.empty_cache()
-    return ytest_hat_NN1, ytest_hat_NN2, ytest_hat_NN3, ytest_hat_NN4, ytest_hat_NN5
+    gc.disable()
+    return ytest_hat_NN1, ytest_hat_NN2, ytest_hat_NN3#, ytest_hat_NN4, ytest_hat_NN5
