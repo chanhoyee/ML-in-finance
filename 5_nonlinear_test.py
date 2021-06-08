9f7b35d1225c7bc44cdfac5af6af7309460e716b
chanhoyee <82632180+chanhoyee@users.noreply.github.com>
2021-06-08 11:23:07 +0900

    Update 5_nonlinear_test.py

--- 5_nonlinear_test.py
+++ 5_nonlinear_test.py
@@ -3,12 +3,12 @@
 
 
 def nonlinear_test(df, xlist, xlist_name):
+    df['ytest_hat_RF' + '_' + xlist_name] = np.nan
+    df['ytest_hat_GBRT' + '_' + xlist_name] = np.nan
     df['ytest_hat_NN1' + '_' + xlist_name] = np.nan
     df['ytest_hat_NN2' + '_' + xlist_name] = np.nan
     df['ytest_hat_NN3' + '_' + xlist_name] = np.nan
-    #df['ytest_hat_NN4' + '_' + xlist_name] = np.nan
-    #df['ytest_hat_NN5' + '_' + xlist_name] = np.nan
-    for test_num in np.arange(0,20):
+    for test_num in np.arange(20):
         gc.disable()
         Xtrain, ytrain, Xval, yval, Xtest, ytest, index_test = make_df(test_num, df, xlist)
         scaler = StandardScaler()
@@ -23,11 +23,10 @@ def nonlinear_test(df, xlist, xlist_name):
         Xtrainval = np.append(Xtrain, Xval, axis = 0)
         ytrainval = np.append(ytrain, yval, axis = 0)
         ps = PredefinedSplit(test_fold=np.append(np.repeat(-1, Xtrain.shape[0]), np.ones(Xval.shape[0])))  
-        ytest_hat_NN1, ytest_hat_NN2, ytest_hat_NN3 = nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest, ps)
-        # ytest_hat_NN1, ytest_hat_NN2, ytest_hat_NN3, ytest_hat_NN4, ytest_hat_NN5 = nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest, ps)
+        ytest_hat_RF ytest_hat_GBRT ytest_hat_NN1, ytest_hat_NN2 = nonlinear_ML(Xtrain, ytrain, Xval, yval, Xtrainval, ytrainval, Xtest, ytest, ps)
+        df.update(pd.DataFrame({'ytest_hat_RF' + '_' + xlist_name: ytest_hat_NN1}, index = index_test))
+        df.update(pd.DataFrame({'ytest_hat_GBRT' + '_' + xlist_name: ytest_hat_NN1}, index = index_test))
         df.update(pd.DataFrame({'ytest_hat_NN1' + '_' + xlist_name: ytest_hat_NN1}, index = index_test))
         df.update(pd.DataFrame({'ytest_hat_NN2' + '_' + xlist_name: ytest_hat_NN2}, index = index_test))
         df.update(pd.DataFrame({'ytest_hat_NN3' + '_' + xlist_name: ytest_hat_NN3}, index = index_test))
-        #df.update(pd.DataFrame({'ytest_hat_NN4' + '_' + xlist_name: ytest_hat_NN4}, index = index_test))
-        #df.update(pd.DataFrame({'ytest_hat_NN5' + '_' + xlist_name: ytest_hat_NN5}, index = index_test))
     return df
